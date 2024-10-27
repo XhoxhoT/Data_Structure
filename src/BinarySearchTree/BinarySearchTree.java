a08024f19319762172e0a2f3c618e0ce2759aecd
XhoXhoT <xhonitinaj@yahoo.com>
2024-10-27 17:33:33 +0100

    BinarySearchTree implementation

---
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,19 @@
+package BinarySearchTree;
+
+public class BinarySearchTree {
+
+     Node root;
+
+    class Node{
+
+        int value;
+        Node left;
+        Node right;
+
+        private Node(int value){
+            this.value=value;
+        }
+    }
+
+
+}
