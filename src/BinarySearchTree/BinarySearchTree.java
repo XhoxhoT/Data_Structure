1fec05cf08d19549e6924d761ca9351cedbaef8a
XhoXhoT <xhonitinaj@yahoo.com>
2024-10-27 17:59:00 +0100

    Insert with while loop

--- src/BinarySearchTree/BinarySearchTree.java
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -15,5 +15,45 @@ public class BinarySearchTree {
         }
     }
 
+    // hapat
+    // create newNode
+    // if root == null then root = newNode
+    // temp = root per iterim ne tree.
+    // while loop
+    // if newNode == temp return false Binary search tree slejon dy nyje me tnjejten vler
+
+    // if < left perndrshe > (me e madhe) right
+    // if null insert newNode lese move to next
+    public boolean insert(int value){
+
+        Node newNode = new Node(value);
+
+        if(root == null){
+            root = newNode;
+            return true;
+        }
+
+        Node temp= root;
+
+        while(true){
+            if(newNode.value == temp.value)
+                return false;
+            if(newNode.value < temp.value){
+                if(temp.left == null){
+                    temp.left=newNode;
+                    return true;
+                }
+                temp=temp.left;
+            }else{
+                if(temp.right == null){
+                    temp.right=newNode;
+                    return true;
+                }
+                temp=temp.right;
+            }
+        }
+
+    }
+
 
 }
